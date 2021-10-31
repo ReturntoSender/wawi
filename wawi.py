2da02d204c5dfbca89cfb0da5e74227a3ba1ce86
ReturntoSender <TerraXF@gmail.com>
2021-11-01 00:25:12 +0100

    Add a bunch

--- wawi.py
+++ wawi.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template
-from werkzeug.utils import escape
+from flask import Flask, render_template, flash
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+app.config["SECRET_KEY"] = "d58bd24dc2e9c3b57492560a5153339a"
 
-@app.route('/<name>')
-def hello(name):
-    return f'Hello, {escape(name)}'
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/item")
+def item():
+    return render_template("item.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
